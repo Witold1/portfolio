cf48856acf3a397a675a64265ba5e831a9052992
Witold1 <Witold1@users.noreply.github.com>
2026-08-13 02:22:09 +0000

    deploy: 302dc34a800d6c43455449c57da37254c7ed7732

---
+++ _next/static/Q-mwSbjiYjRAGOfvjjvFy/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[...slug]","\u002Fgallery","\u002Fprojects","\u002Fprojects\u002F[...slug]","\u002Fsitemap"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
